f0bcc7695305a2fc28bdec8dc61bb2dbbed30eb0
sushilsree98 <sushil.sree98@gmail.com>
2022-12-27 22:40:18 +0530

    Orbit, Transform and Pivot Controls init

--- drei/src/Experience.js
+++ drei/src/Experience.js
@@ -1,28 +1,34 @@
-import { useThree, extend } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
-extend({ OrbitControls })
-
+import { OrbitControls, TransformControls, PivotControls } from '@react-three/drei'
+import { useRef } from 'react'
 export default function Experience()
 {
-    const { camera, gl } = useThree()
-
+    const box = useRef()
     return <>
 
-        <orbitControls args={ [ camera, gl.domElement ] } />
-
+        <OrbitControls makeDefault/>
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <PivotControls
+            anchor={[0, 0, 0]}
+            activeAxes={[1, 0, 1]}
+            depthTest={false}
+            scale={100}
+            fixed={true}
+            autoTransform={false}
+            >
+            <mesh position-x={ - 2 }>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+        </PivotControls>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={box} position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
+
+        <TransformControls object={box} showY={false}/>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
