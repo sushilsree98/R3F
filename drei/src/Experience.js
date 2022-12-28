f6bcd37e569104f5bb9f973551190dc89ecbad6a
sushilsree98 <sushil.sree98@gmail.com>
2022-12-28 23:00:00 +0530

    Drei helpers

--- drei/src/Experience.js
+++ drei/src/Experience.js
@@ -1,8 +1,9 @@
-import { OrbitControls, TransformControls, PivotControls } from '@react-three/drei'
+import { OrbitControls, TransformControls, PivotControls, Html, Text, Float, MeshReflectorMaterial } from '@react-three/drei'
 import { useRef } from 'react'
 export default function Experience()
 {
     const box = useRef()
+    const sphere = useRef()
     return <>
 
         <OrbitControls makeDefault/>
@@ -15,9 +16,8 @@ export default function Experience()
             depthTest={false}
             scale={100}
             fixed={true}
-            autoTransform={false}
             >
-            <mesh position-x={ - 2 }>
+            <mesh position-x={ - 2 } ref={sphere}>
                 <sphereGeometry />
                 <meshStandardMaterial color="orange" />
             </mesh>
@@ -26,14 +26,35 @@ export default function Experience()
         <mesh ref={box} position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
+            <Html
+                position={[1,1,0]}
+                wrapperClass="label"
+                center
+                distanceFactor={8}
+                occlude={[sphere, box]}
+            >Cube</Html>
         </mesh>
 
         <TransformControls object={box} showY={false}/>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh  position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
+            {/* <meshStandardMaterial color="greenyellow" /> */}
+            <MeshReflectorMaterial
+                color={'greenyellow'}
+            />
         </mesh>
+
+        <Float>
+            <Text
+                font='./bangers-v20-latin-regular.woff'
+                fontSize={1}
+                color={'salmon'}
+                position-y={2}
+            >I Love R3F
+            </Text>
+
+        </Float>
 
     </>
 }
