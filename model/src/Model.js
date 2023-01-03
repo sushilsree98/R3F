46e630a2549400b881d286d13dec927fedca60c6
sushilsree98 <sushil.sree98@gmail.com>
2023-01-03 22:11:17 +0530

    Drei model loader added

--- model/src/Model.js
+++ model/src/Model.js
@@ -2,16 +2,25 @@
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { Clone, useGLTF } from '@react-three/drei'
 
 export default function Model() {
-    const model = useLoader(GLTFLoader,'./FlightHelmet/glTF/FlightHelmet.gltf',
-        (loader)=>{
-            const dracoLoader = new DRACOLoader();
-            dracoLoader.setDecoderPath('./draco/');
-            loader.setDRACOLoader(dracoLoader)
-        }
-    ) 
+    // const model = useLoader(GLTFLoader,'./FlightHelmet/glTF/FlightHelmet.gltf',
+    //     (loader)=>{
+    //         const dracoLoader = new DRACOLoader();
+    //         dracoLoader.setDecoderPath('./draco/');
+    //         loader.setDRACOLoader(dracoLoader)
+    //     }
+    // ) 
 
-    return <primitive object={model.scene} scale={5} position-y={-1}/>
+    const model = useGLTF('./hamburger.glb') 
+
+    return <>
+            <Clone object={model.scene} scale={0.35} position-y={-1}/>
+            <Clone object={model.scene} scale={0.35} position-y={0}/>
+            <Clone object={model.scene} scale={0.35} position-y={1}/>
+        </>
 
 }
+
+useGLTF.preload('./hamburger.glb')
